4d9969d0ecd3373bc9c63729d48986c57c447e7f
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-07-07 15:09:39 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+// Task 1: customerName
+var customerName = 'bob';
+
+// Task 2: upperCaseCustomerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Task 3: setBestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Task 4: overwriteBestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Task 5: leastFavoriteCustomer and changeLeastFavoriteCustomer
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; // This will throw an error
+}
